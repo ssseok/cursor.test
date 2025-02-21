8fc82b1761a7e5549af1b2ddbaa4a60b2fcb4bf8
Hyun Seok Park <luckseok1@gmail.com>
2025-02-21 19:03:29 +0900

    init: project setting

---
+++ app/common/pages/home-page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+    return (
+        <main className="container mx-auto px-4 py-8">
+            <h1 className="text-4xl font-bold mb-6">홈페이지</h1>
+        </main>
+    );
+}

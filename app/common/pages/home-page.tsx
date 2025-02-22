61ab7b35a4d6358ce72dcae1ce30432b42e84798
Hyun Seok Park <luckseok1@gmail.com>
2025-02-23 02:44:22 +0900

    design: Navigation UI

--- app/common/pages/home-page.tsx
+++ app/common/pages/home-page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '../components/ui/button';
+
 export default function HomePage() {
     return (
         <main className="container mx-auto px-4 py-8">
             <h1 className="text-4xl font-bold mb-6">홈페이지</h1>
+            <Button>Click me</Button>
         </main>
     );
 }
--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,7 @@ import {
 
 import type { Route } from './+types/root';
 import './app.css';
+import Navigation from './common/components/ui/navigation';
 
 export const links: Route.LinksFunction = () => [
     { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
@@ -25,7 +26,10 @@ export const links: Route.LinksFunction = () => [
 
 export function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang="en">
+        <html
+            lang="en"
+            className="dark"
+        >
             <head>
                 <meta charSet="utf-8" />
                 <meta
@@ -45,7 +49,12 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-    return <Outlet />;
+    return (
+        <>
+            <Navigation />
+            <Outlet />
+        </>
+    );
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {

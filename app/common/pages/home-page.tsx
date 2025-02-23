f8e3cb7e3690d976572334f8ebd14e9057cd7b2d
Hyun Seok Park <luckseok1@gmail.com>
2025-02-24 06:01:43 +0900

    design: Post Card UI

--- app/common/pages/home-page.tsx
+++ app/common/pages/home-page.tsx
@@ -1,6 +1,8 @@
-import { type MetaFunction } from 'react-router';
+import { Link, type MetaFunction } from 'react-router';
 
 import { ProductCard } from '~/features/products/components/product-card';
+import { Button } from '../components/ui/button';
+import { PostCard } from '~/features/community/components/post-card';
 
 export const meta: MetaFunction = () => {
     return [
@@ -11,17 +13,26 @@ export const meta: MetaFunction = () => {
 
 export default function HomePage() {
     return (
-        <div className="px-20">
+        <div className="px-20 space-y-40">
             <div className="grid grid-cols-3 gap-4">
                 <div>
-                    <h2 className="text-5xl font-bold leading-tight tracking-tight">
+                    <h2 className="text-3xl font-bold leading-tight tracking-tight">
                         Today's Products
                     </h2>
-                    <p className="text-xl font-light text-foreground">
+                    <p className="text-lg font-light text-foreground">
                         The best products made by our community today.
                     </p>
+                    <Button
+                        variant="link"
+                        asChild
+                        className="text-base p-0"
+                    >
+                        <Link to="/products/leaderboards">
+                            Explore all products &rarr;
+                        </Link>
+                    </Button>
                 </div>
-                {Array.from({ length: 10 }).map((_, index) => (
+                {Array.from({ length: 11 }).map((_, index) => (
                     <ProductCard
                         id={`productId-${index}`}
                         name="Product Name"
@@ -29,6 +40,36 @@ export default function HomePage() {
                         commentsCount={12}
                         viewsCount={12}
                         votesCount={120}
+                        key={index}
+                    />
+                ))}
+            </div>
+            <div className="grid grid-cols-3 gap-4">
+                <div>
+                    <h2 className="text-3xl font-bold leading-tight tracking-tight">
+                        Latest Discussions
+                    </h2>
+                    <p className="text-lg font-light text-foreground">
+                        The latest discussions from our community.
+                    </p>
+                    <Button
+                        variant="link"
+                        asChild
+                        className="text-base p-0"
+                    >
+                        <Link to="/community">
+                            Explore all discussions &rarr;
+                        </Link>
+                    </Button>
+                </div>
+                {Array.from({ length: 11 }).map((_, index) => (
+                    <PostCard
+                        id={`postId-${index}`}
+                        title="What is the best productivity tool?"
+                        author="Nico"
+                        authorAvatarUrl="https://github.com/apple.png"
+                        category="Productivity"
+                        postedAt="12 hours ago"
                         key={index}
                     />
                 ))}
